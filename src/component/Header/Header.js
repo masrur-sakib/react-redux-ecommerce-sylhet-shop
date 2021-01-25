3a3a7e87e101d6c267536b70e30999af19f5dddf
Masrur Sakib <mashrur333@gmail.com>
2021-01-26 01:13:37 +0600

    Task Environment Setup 2

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import './Header.css';
+import siteLogo from '../../images/siteLogo.png';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header Component</h1>
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <img src={siteLogo} alt="" width="220" height="70"></img>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-collapse collapse justify-content-between" id="navbarNavAltMarkup">
+                        <div>
+                            <form className="d-flex">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            </form>
+                        </div>
+                        <div>
+                            <ul className="navbar-nav">
+                                <li className="nav-item active">
+                                    <a className="nav-link" aria-current="true" href="#home">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#about">About</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#shop">Shop</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#help">Help</a>
+                                </li>
+                                <li className="nav-item">
+                                    <button type="button" className="btn btn-secondary"><FaShoppingCart /> Your Cart</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
--- src/component/MockupOne/Home/Home.js
+++ src/component/MockupOne/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Home.css';
+import TopSection from '../TopSection/TopSection';
+import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
+import Newsletter from '../Newsletter/Newsletter';
+import ProductsGrid from '../ProductsGrid/ProductsGrid';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div className="container">
             <h1>Home Component</h1>
+            <TopSection></TopSection>
+            <FeaturedProducts></FeaturedProducts>
+            <Newsletter></Newsletter>
+            <ProductsGrid></ProductsGrid>
+            <About></About>
         </div>
     );
 };
